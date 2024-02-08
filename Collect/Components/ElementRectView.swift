8c93259f636f91e0102244d8b4585ac8d596693b
Stef Kors <stef.kors@gmail.com>
2024-02-08 13:49:17 +0100

    Update ElementRectView.swift

--- Collect/Components/ElementRectView.swift
+++ Collect/Components/ElementRectView.swift
@@ -7,6 +7,86 @@
 
 import SwiftUI
 
+struct PerspectiveView: View {
+    @State private var point = CGPoint(x: 0, y: 0)
+    @State private var degrees: Double = 0
+    @State private var draging: Bool = false
+
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+
+                ElementRectView(label: "group", level: 0, origin: .init(x: 120, y: 120), size: CGSize(width: 200, height: 80), showCollect: true)
+                    .rotation3DEffect(
+                        .degrees(degrees),
+                        axis: (x: point.x, y: point.y, z: 0),
+                        anchor: .center,
+                        anchorZ: 0,
+                        perspective: 1
+                    )
+                ElementRectView(label: "text", level: 1, origin: .init(x: 140, y: 160), size: CGSize(width: 100, height: 30), showCollect: true)
+                    .rotation3DEffect(
+                        .degrees(degrees),
+                        axis: (x: point.x, y: point.y, z: 0),
+                        anchor: .center,
+                        anchorZ: -50,
+                        perspective: 1
+                    )
+                ElementRectView(label: "text", level: 2, origin: .init(x: 260, y: 150), size: CGSize(width: 100, height: 30), showCollect: true)
+                    .rotation3DEffect(
+                        .degrees(degrees),
+                        axis: (x: point.x, y: point.y, z: 0),
+                        anchor: .center,
+                        anchorZ: -100,
+                        perspective: 1
+                    )
+            }
+
+                .simultaneousGesture(
+                    DragGesture(minimumDistance: 0)
+                        .onChanged({ gesture in
+                            let centerX = proxy.size.width / 2
+                            let centerY = proxy.size.height / 2
+
+                            let x = 0 - (gesture.location.y / centerY - 1)
+                            let y = (gesture.location.x / centerX - 1)
+
+                            let x1 = gesture.location.x - centerX
+                            let y1 = gesture.location.y - centerY
+
+                            let range = sqrt(x1 * x1 + y1 * y1)
+                            let degreesFactor = range / sqrt(2 * centerX * centerX)
+
+                            withAnimation {
+                                point = CGPoint(x: x, y: y)
+                                degrees = Double(30 * degreesFactor.clamped(0, 1))
+                            }
+                            draging = true
+                        })
+                        .onEnded(onDragEndedAction(gesture:))
+                )
+        }
+
+    }
+    private func onDragEndedAction(gesture: DragGesture.Value) -> Void {
+        withAnimation {
+            point = CGPoint(x: 0, y: 0)
+            degrees = 0
+        }
+        draging = false
+    }
+}
+
+fileprivate extension Comparable {
+    func clamped(_ f: Self, _ t: Self)  ->  Self {
+        var r = self
+        if r < f { r = f }
+        if r > t { r = t }
+        return r
+    }
+}
+
+
 struct ElementRectView: View {
     var label: String? = nil
     var level: Int = 0
@@ -17,7 +97,7 @@ struct ElementRectView: View {
     private let radius: CGFloat = 8
 
     private var roundLeadingCorner: Bool {
-        (level != 0) || (label == nil)
+        (label == nil)
     }
 
     private var shape: UnevenRoundedRectangle {
@@ -85,6 +165,10 @@ struct ElementRectView: View {
 }
 
 #Preview {
+    PerspectiveView()
+}
+
+#Preview {
     ZStack {
         ElementRectView(origin: .init(x: 20, y: 20), size: CGSize(width: 200, height: 80), showCollect: true)
 
@@ -92,6 +176,10 @@ struct ElementRectView: View {
         ElementRectView(label: "text", level: 1, origin: .init(x: 300, y: 160), size: CGSize(width: 200, height: 80), showCollect: true)
         ElementRectView(label: "text", level: 2, origin: .init(x: 300, y: 160), size: CGSize(width: 200, height: 80), showCollect: true)
         ElementRectView(label: "text", level: 3, origin: .init(x: 300, y: 160), size: CGSize(width: 200, height: 80), showCollect: true)
+
+        ElementRectView(label: "group", level: 0, origin: .init(x: 20, y: 160), size: CGSize(width: 200, height: 80), showCollect: true)
+        ElementRectView(label: "text", level: 1, origin: .init(x: 40, y: 200), size: CGSize(width: 100, height: 30), showCollect: true)
+        ElementRectView(label: "text", level: 2, origin: .init(x: 100, y: 180), size: CGSize(width: 100, height: 30), showCollect: true)
     }
 
 }
